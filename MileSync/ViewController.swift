8349a51f17cfd149f3f67ec0f99e9bf54209a649
JaiminRaval <siliconvalley.jaimin@gmail.com>
2023-12-24 16:19:56 +0530

    Initial Commit

---
+++ MileSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MileSync
+//
+//  Created by Jaimin Raval on 24/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
